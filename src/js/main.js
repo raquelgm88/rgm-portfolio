77d04450c2f9fdfcb5db71f1cda7849c490b7059
raquelgm88 <raquel.rgm7@gmail.com>
2023-05-31 17:32:31 +0200

    Add lateral scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const leftBtn = document.querySelector('.fa-circle-chevron-left');
+const rightBtn = document.querySelector('.fa-circle-chevron-right');
+const content = document.querySelector('.projects__all');
+
+const handleClickLeft = () => {
+    content.scrollRight +=800;
+}
+
+leftBtn.addEventListener('click', handleClickLeft);
+rightBtn.addEventListener('click', handleClickRight);
